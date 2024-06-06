40e6781cac4f69ec0df841a72ecd95a460ab1e97
Bhaskar Kaura <bhaskar.kaura@thewitslab.com>
2024-06-06 13:26:19 +0530

    feat: Applied header interceptor in Course, scholarship

--- protocol-layer/src/modules/scholarship/scholarship.module.ts
+++ protocol-layer/src/modules/scholarship/scholarship.module.ts
@@ -3,10 +3,11 @@ import { ScholarshipService } from './services/scholarship.serviceV1';
 import { ScholarshipController } from './scholarship.controllerV1';
 import { AxiosService } from '../../common/axios/axios.service';
 import { CommonModule } from '../../common/common.module';
+import { HeaderInterceptorService } from '../../common/network/header.interceptor';
 
 @Module({
   imports: [CommonModule],
   controllers: [ScholarshipController],
-  providers: [ScholarshipService, AxiosService],
+  providers: [ScholarshipService, AxiosService, HeaderInterceptorService],
 })
 export class ScholarshipModule {}
